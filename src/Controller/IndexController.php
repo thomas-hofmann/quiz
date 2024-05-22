43385e7a23562dfe063516ab22a2639f843b8042
Thomas Hofmann <thomas.hofmann@h-da.de>
2024-05-22 16:04:15 +0200

    initial symfony setup. webpack, jquery, sass, bootstrap installed.

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class IndexController extends AbstractController {
+	#[Route('/')]
+	public function indexAction(): Response {
+		return $this->render('index.html.twig', [
+		]);
+	}
+}
