c26196fa92dbfe8193818c17bb4f273adb0454c0
Thomas Hofmann <thomas.hofmann@h-da.de>
2024-05-24 11:43:25 +0200

    updated quizcontroller

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -12,7 +12,6 @@ use App\Entity\Quiz;
 use App\Entity\LeaderBoardEntry;
 use Doctrine\ORM\EntityManagerInterface;
 
-#[IsGranted('ROLE_USER')]
 class QuizController extends AbstractController {
     #[Route('/quiz', name: 'quiz')]
     public function quizAction(Request $request, EntityManagerInterface $entityManager): Response {
@@ -45,17 +44,6 @@ class QuizController extends AbstractController {
             'matrikelnummer' => $matrikelnummer,
             'index' => $index,
             'rightIndex' => $rightIndex,
-        ]);
-    }
-
-    #[Route('/list', name: 'list')]
-    public function listAction(EntityManagerInterface $entityManager): Response {
-
-        $quizRepository = $entityManager->getRepository(Quiz::class);
-
-        $quizzes = $quizRepository->findAll();
-        return $this->render('list.html.twig', [
-            'quizzes' => $quizzes
         ]);
     }
 
@@ -138,54 +126,6 @@ class QuizController extends AbstractController {
             'matrikelnummer' => $matrikelnummer,
             'rightIndex' => $rightIndex,
             'leaderBoardEntries' => $leaderBoardEntries
-        ]);
-    }
-
-    #[Route('/create-quiz', name: 'create-quiz')]
-    public function createQuizAction(Request $request, EntityManagerInterface $entityManager): Response {
-        if($request->get('quizname')) {
-            $quiz = new Quiz();
-            $quiz->setName($request->get('quizname'));
-            $code = uniqid();
-            $quiz->setCode($code);
-            $entityManager->persist($quiz);
-            $entityManager->flush();
-            return $this->redirectToRoute('create-questions', ['code' => $code]);
-        }
-
-        return $this->render('create-quiz.html.twig', [
-        ]);
-    }
-
-    #[Route('/create-questions', name: 'create-questions')]
-    public function createQuestionsAction(Request $request, EntityManagerInterface $entityManager): Response {
-        if($request->get('code')) {
-            $quizRepository = $entityManager->getRepository(Quiz::class);
-            $quiz = $quizRepository->findOneBy(['code' => $request->get('code')]);
-        }
-
-        if($quiz && $request->get('question')) {
-            $question = new Question();
-            $question->setText($request->get('question'));
-
-            $question->setAnswerOne($request->get('answer1'));
-            $question->setAnswerTwo($request->get('answer2'));
-            $question->setAnswerThree($request->get('answer3'));
-            $question->setAnswerFour($request->get('answer4'));
-
-            $question->setAnswerRight($request->get('rightAnswer'));
-
-            $question->setQuiz($quiz);
-
-            $entityManager->persist($question);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('create-questions', ['code' => $quiz->getCode()]);
-        }
-
-        return $this->render('create-questions.html.twig', [
-            'code' => $request->get('code'),
-            'questions' => $quiz->getQuestions()
         ]);
     }
 }
