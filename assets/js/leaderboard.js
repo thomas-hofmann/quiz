614477ac5d96870eb581cc1dac77b3e6eb887e6c
Thomas Hofmann <thomas.hofmann@h-da.de>
2024-06-19 13:48:00 +0200

    Some styling fpr statistics

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -20,14 +20,18 @@ $(function() {
     $('#show-answers-button').on('click', function(event) {
         event.preventDefault();  // Verhindert das Standardverhalten des Links
         var url = $(this).attr('data-href');  // Holt die URL aus dem href-Attribut des Links
-
+        $('#show-answers-button').html('<i class="fa-solid fa-spinner fa-spin"></i></i> Ergebnisse aktualisieren');
         $.ajax({
             url: url,
             method: 'GET',
             success: function(data) {
                 const newContent = $(data).find('#answer-stats-container').html();
                 $('#answer-stats-container').html(newContent);
-                $('#show-answers-button').html('<i class="fa-solid fa-arrows-rotate"></i> Aktualisieren');
+                $('#show-answers-button .fa-solid').fadeOut(300, function() {
+                    $('#show-answers-button').html('<i class="fa-solid fa-arrows-rotate"></i> Ergebnisse aktualisieren');
+                    $(this).fadeIn(300);
+                });
+                
             }
         });
     });
