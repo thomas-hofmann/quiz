988c3577b83d33283c154e22a8497105108e088f
Thomas Hofmann <thomas.hofmann@h-da.de>
2024-10-30 15:00:13 +0100

    Fixed migrations

--- migrations/Version20241030135925.php
+++ migrations/Version20241030135925.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241029145109 extends AbstractMigration
+final class Version20241030135925 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,6 +20,8 @@ final class Version20241029145109 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_64C19C1A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE category ADD CONSTRAINT FK_64C19C1A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE quiz ADD category_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE quiz ADD CONSTRAINT FK_A412FA9212469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
         $this->addSql('CREATE INDEX IDX_A412FA9212469DE2 ON quiz (category_id)');
@@ -29,6 +31,8 @@ final class Version20241029145109 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE quiz DROP FOREIGN KEY FK_A412FA9212469DE2');
+        $this->addSql('ALTER TABLE category DROP FOREIGN KEY FK_64C19C1A76ED395');
+        $this->addSql('DROP TABLE category');
         $this->addSql('DROP INDEX IDX_A412FA9212469DE2 ON quiz');
         $this->addSql('ALTER TABLE quiz DROP category_id');
     }
