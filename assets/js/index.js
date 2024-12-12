49e4d827c67e70e5590f227d6791d47edb011280
Thomas Hofmann <thomas.hofmann@h-da.de>
2024-12-12 11:20:46 +0100

    fixed issue

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,25 @@
-
 $(function() {
     const input = document.getElementById('codeInput');
-    const basePlaceholder = "Beitrittscode";
-    let charCount = 1; // Start bei 1, um mit "B" zu beginnen
-    let increment = true;
 
-    const intervalId = setInterval(() => {
-    // Erstelle den Placeholder mit dem fortlaufenden Wort
-    input.setAttribute('placeholder', basePlaceholder.slice(0, charCount));
+    // Prüfen, ob das Element existiert
+    if (input) {
+        const basePlaceholder = "Beitrittscode";
+        let charCount = 1; // Start bei 1, um mit "B" zu beginnen
+        let increment = true;
 
-    // Buchstaben hochzählen
-    if (increment) {
-        charCount++;
-        if (charCount > basePlaceholder.length) {
-        increment = false; // Wenn das ganze Wort erreicht ist, stoppen
-        clearInterval(intervalId); // Stoppe die Animation
-        }
+        const intervalId = setInterval(() => {
+            // Erstelle den Placeholder mit dem fortlaufenden Wort
+            input.setAttribute('placeholder', basePlaceholder.slice(0, charCount));
+
+            // Buchstaben hochzählen
+            if (increment) {
+                charCount++;
+                if (charCount > basePlaceholder.length) {
+                    increment = false; // Wenn das ganze Wort erreicht ist, stoppen
+                    clearInterval(intervalId); // Stoppe die Animation
+                }
+            }
+
+        }, 75); // Intervall von 75ms
     }
-
-    }, 75); // Intervall von 500ms
 });
